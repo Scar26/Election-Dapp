0d1a05a955db4ad991c67d5286e4f35ab0eae617
Scar26 <mmatty26@gmail.com>
2019-07-21 00:10:42 +0530

    readme removed

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,8 +1,23 @@
 pragma solidity ^0.5.0;
 
 contract Election{
-  string public candidate;
+
+  struct Candidate{
+    uint id;
+    string name;
+    uint votes;
+  }
+
+  mapping(uint => Candidate) public candidates;
+  uint public cancount;
+
   constructor() public{
-    candidate = "Black Donald trump";
+    newCandidate("Black Donald Trump");
+    newCandidate("Keanu Reaves");
+  }
+
+  function newCandidate(string memory _name) private{
+    cancount ++;
+    candidates[cancount] = Candidate(cancount, _name, 0);
   }
 }
