6c0cd83a8e89789d97bd22fdd87ec660811d01d9
Scar26 <mmatty26@gmail.com>
2019-08-27 15:19:18 +0530

    realtime updates

--- API-server/server.js
+++ API-server/server.js
@@ -11,17 +11,18 @@ const CosmosClient = require('@azure/cosmos').CosmosClient;
 const config = require('./config');
 const endpoint = config.endpoint;
 const key = config.key;
+const request = require('request')
 
 const IV_LENGTH = 16; // For AES, this is always 16
 const secert = 'm1cr0s0ft_z1nd4b4d_h3nt41_h4v3n_';
 function encrypt(text, ENCRYPTION_KEY) {
- let iv = crypto.randomBytes(IV_LENGTH);
- let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), iv);
- let encrypted = cipher.update(text);
+   let iv = crypto.randomBytes(IV_LENGTH);
+   let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), iv);
+   let encrypted = cipher.update(text);
 
- encrypted = Buffer.concat([encrypted, cipher.final()]);
+   encrypted = Buffer.concat([encrypted, cipher.final()]);
 
- return iv.toString('hex') + ':' + encrypted.toString('hex');
+   return iv.toString('hex') + ':' + encrypted.toString('hex');
 }
 
 function decrypt(text, ENCRYPTION_KEY) {
@@ -44,18 +45,45 @@ app.use(parser.urlencoded({ extended: true}));
 if (typeof web3 !== 'undefined') { web3 = new Web3(web3.currentProvider); }
 else { web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545')); }
 
-abi = '[  {    "constant": true,    "inputs": [      {        "name": "",        "type": "uint256"      }    ],    "name": "candidates",    "outputs": [      {        "name": "id",        "type": "uint32"      },      {        "name": "name",        "type": "string"      },      {        "name": "votes",        "type": "uint256"      }    ],    "payable": false,    "stateMutability": "view",    "type": "function"  },  {    "constant": true,    "inputs": [],    "name": "cancount",    "outputs": [      {        "name": "",        "type": "uint32"      }    ],    "payable": false,    "stateMutability": "view",    "type": "function"  },  {    "inputs": [],    "payable": false,    "stateMutability": "nonpayable",    "type": "constructor"  },  {    "anonymous": false,    "inputs": [      {        "indexed": false,        "name": "response",        "type": "string"      }    ],    "name": "temp",    "type": "event"  },  {    "constant": false,    "inputs": [      {        "name": "vid",        " ": "uint256"      },      {        "name": "cid",        "type": "uint256"      }    ],    "name": "castVote",    "outputs": [      {        "name": "",        "type": "string"      }    ],    "payable": false,    "stateMutability": "nonpayable",    "type": "function"  },  {    "constant": false,    "inputs": [      {        "name": "queryID",        "type": "bytes32"      },      {        "name": "result",        "type": "string"      }    ],    "name": "__callback",    "outputs": [],    "payable": false,    "stateMutability": "nonpayable",    "type": "function"  },  {    "constant": false,    "inputs": [      {        "name": "_myid",        "type": "bytes32"      },      {        "name": "_result",        "type": "string"      },      {        "name": "_proof",        "type": "bytes"      }    ],    "name": "__callback",    "outputs": [],    "payable": false,    "stateMutability": "nonpayable",    "type": "function"  },  {    "constant": false,    "inputs": [],    "name": "testOracle",    "outputs": [],    "payable": true,    "stateMutability": "payable",    "type": "function"  }]';
+abi = '[  {    "constant": true,    "inputs": [      {        "name": "",        "type": "uint256"      }    ],    "name": "candidates",    "outputs": [      {        "name": "id",        "type": "uint32"      },      {        "name": "name",        "type": "string"      },      {        "name": "votes",        "type": "uint256"      }    ],    "payable": false,    "stateMutability": "view",    "type": "function",    "signature": "0x3477ee2e"  },  {    "constant": true,    "inputs": [],    "name": "cancount",    "outputs": [      {        "name": "",        "type": "uint32"      }    ],    "payable": false,    "stateMutability": "view",    "type": "function",    "signature": "0xb865d909"  },  {    "inputs": [],    "payable": false,    "stateMutability": "nonpayable",    "type": "constructor",    "signature": "constructor"  },  {    "constant": false,    "inputs": [      {        "name": "vid",        "type": "uint256"      },      {        "name": "cid",        "type": "uint256"      }    ],    "name": "castVote",    "outputs": [      {        "name": "",        "type": "string"      }    ],    "payable": false,    "stateMutability": "nonpayable",    "type": "function",    "signature": "0x2c0a3f89"  }]';
 
 abiobj = JSON.parse(abi);
 var defaultAccount = '0xEcebc654DC57ee532a63Ea6F2634D186963E7b24'; // Has to be updated according to the local blockchain
-var contract_addr = '0x151512B893E5D418D1CB2d6bF77f0aB77CEc6200'; //Has to be updated everytime the contract is migrated, can be obtained via Election.deployed().address in the truffle console
+var contract_addr = '0xA14b545a3d710c04bb8B6607f76287c86D7977C1'; //Has to be updated everytime the contract is migrated, can be obtained via Election.deployed().address in the truffle console
 election = new web3.eth.Contract(abiobj, contract_addr , {from : defaultAccount});
 
 console.log('web3 initialized');
 constituencies = JSON.parse(fs.readFileSync('constituencies.json')); //We decided to go with json for storing the constituencies list because it will be faster to have the entire tree as a state variable of the server, and this is all public information anyway.
+// allowed_machines = JSON.parse(fs.readFileSync('ips.json')); //list of whitelisted ips
 concount = constituencies.length;
 console.log(constituencies);
 console.log(concount);
+
+function verify(v,f) {
+	console.log("Verify")
+	var url = 'https://votesec-cosmos-api.herokuapp.com/test';
+	var json = {vid: v, fprint: f}
+
+	request.post(
+		url,
+		{ form: { data: encrypt(JSON.stringify(json)), key:secert } }, //DUMMY ADD ENCRYPTION
+		function (error, response, body) {
+			console.log("in anon fxn")
+			console.log(response)
+			console.log(error)
+			if (!error && response.statusCode == 200) {
+				console.log(body);
+				if (body == 'VALID') {
+					return true;
+				}
+				else{
+					return false;
+				}
+			}
+		}
+	);
+}
+
 app.post('/getcans',function(req,res){
     if(typeof req.body.pin == 'string'){
       pin = req.body.pin;
@@ -82,34 +110,19 @@ app.post('/getcans',function(req,res){
 });
 
 app.post('/voteapi',function(req,res){
-  obj = JSON.parse(decrypt(req.body.data,secert));
-  var vid = obj.vid;
-  var cid = obj.cid;
-  election.methods.castVote(78, 1).send({from : defaultAccount},function(e,r){ console.log(r) });
-});
-
-const client = new CosmosClient({ endpoint, key });
-
-async function queryContainer(val) {
-  const querySpec = {
-     query: 'SELECT * FROM voters r WHERE r.id = @vid',
-     parameters: [
-         {
-             name: "@vid",
-             value: val
-         }
-     ]
- };
-
- const { resources } = await client.database('voters').container('voters').items.query(querySpec, {enableCrossPartitionQuery:true}).fetchAll();
- console.log(resources[0].name);
- result = resources[0];
- return result;
-};
-
-app.post('/verify',function(req,res){
-  var vid = req.body.vid;
-  var con = req.body.con;
-  details = queryContainer(vid);
-
+  // if(allowed_machines.includes(request.connection.remoteAddress.toString())){
+    vid = JSON.parse(decrypt(req.body.data,secert)).vid;
+    cid = JSON.parse(decrypt(req.body.data,secert)).cid;
+    var md5sum = crypto.createHash('md5');
+    md5sum.update(cid);
+    cid = md5sum.digest('hex');
+    m5sum = null;
+    fprint = JSON.parse(decrypt(req.body.data,secert)).fprint;
+    election.methods.castVote(vid, cid).send({from : defaultAccount},function(e,r){ console.log(r) });
+    details = queryContainer(vid);
+    verification = verify(vid,fprint);
+   // }
+  // else{
+  //   res.status(404).send("Something went wrong");
+  // }
 });
--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,54 +1,36 @@
 pragma solidity ^0.5.0;
-import "./provable.sol";
 
-contract Election is usingProvable{
-
-  address private _owner;
-  string private secert = "f4Q2t@1$^";
-  string public voterRecord;
-  string public ETHUSD;
-  modifier onlyOwner {
-    require(msg.sender == _owner);
-    _;
-  }
+contract Election{
 
   struct Candidate{
     uint32 id;
     string name;
     uint votes;
   }
-  event LogConstructorInitiated(string nextStep);
-   event LogPriceUpdated(string price);
-   event LogNewProvableQuery(string description);
-
-
   mapping(uint => bool) private voted;
   mapping(uint => Candidate) public candidates;
   uint32 public cancount;
+  address private _owner;
 
-  event temp(string response);
+  modifier onlyOwner {
+    require(msg.sender == _owner);
+    _;
+  }
 
   constructor() public{
-    OAR = OracleAddrResolverI(0xe273B3DB6Ec4aB4869C9aE92A3AfD6A770Ce68B5);
     _owner = msg.sender;
+  	newCandidate('Bajendra Modi');
+  	newCandidate('Pappu');
+  	newCandidate('Jejriwal');
   }
 
-  function verifyVoter(uint voterid) private returns(bool){
-    return true;
-  }
+  function castVote(uint vid, uint cid) public onlyOwner returns(string memory){
 
-  function castVote(uint vid, uint cid) public returns(string memory){
-    if (verifyVoter(vid) == false){
-      return "Not a valid Voter ID";
-    }
-
-    else{
       if(voted[vid] != true){
         voted[vid] = true;
         recordVote(cid);
       }
       return "Your vote has been recorded successfully";
-  }
 }
 
   function newCandidate(string memory _name) private{
@@ -60,14 +42,8 @@ contract Election is usingProvable{
     candidates[_id].votes++;
   }
 
-    function __callback(bytes32 myid, string memory result) public{
-        if (msg.sender != provable_cbAddress()) revert();
-        ETHUSD = result;
-        emit LogPriceUpdated(result);
-     }
-
-  function testOracle() public payable{
-    emit LogNewProvableQuery("Provable query was sent, standing by for the answer..");
-    provable_query("URL", "json(https://api.pro.coinbase.com/products/ETH-USD/ticker).price");
-  }
+  function concludeElection() onlyOwner private returns(bool success) {
+  _owner = address(0);
+  return true;
+}
 }
